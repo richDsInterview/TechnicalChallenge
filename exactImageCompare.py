2ccbff330d3389f4ec4de2773e6e8d17f64299aa
Richard Armstrong <richarms@ska.ac.za>
2018-04-19 14:27:58 +0200

    random testing argv shit

--- exactImageCompare.py
+++ exactImageCompare.py
@@ -21,8 +21,7 @@ def main(arguments):
     args = parser.parse_args(arguments)
 
     print(args)
-    print(args[1])
-    print(args['infile1'])
+    args
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
